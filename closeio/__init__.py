b6041f1b58ca6271b3d3869d1dadf875e6f179cc
Denis Cornehl <syphar@fastmail.fm>
2015-09-10 15:52:49 +0200

    bump version

--- closeio/__init__.py
+++ closeio/__init__.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 
 __author__ = 'Denis Cornehl'
 __email__ = 'denis.cornehl@gmail.com'
-__version__ = '0.1.35'
+__version__ = '0.1.36'
 
 
 from .closeio import CloseIO, CloseIOError  # noqa
