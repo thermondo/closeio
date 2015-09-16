6d4a29789851ab7b5155dac4a75c7cfe334fbd64
Denis Cornehl <syphar@fastmail.fm>
2015-09-16 08:53:58 +0200

    bump version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ class PyTest(Command):
 
 setup(
     name='faster_closeio',
-    version='0.1.38',
+    version='0.1.39',
     description='Slim API wrapper to access close.io CRM',
     long_description=readme + '\n\n' + history,
     author='Denis Cornehl',
