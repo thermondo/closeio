32d22c44d6777266908f95f79892710f215b5c0e
Denis Cornehl <syphar@fastmail.fm>
2015-10-30 14:55:37 +0100

    better py2/3 compatibility
    Closed #18

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ class PyTest(Command):
 
 setup(
     name='faster_closeio',
-    version='0.1.39',
+    version='0.1.40',
     description='Slim API wrapper to access close.io CRM',
     long_description=readme + '\n\n' + history,
     author='Denis Cornehl',
