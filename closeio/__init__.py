8d437bb29369dab4bacf20e8955112a7a1f9975d
Johannes Hoppe <info@johanneshoppe.com>
2017-11-25 18:52:23 +0100

    Switch to pbr

--- closeio/__init__.py
+++ closeio/__init__.py
@@ -2,10 +2,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-__author__ = 'Denis Cornehl'
-__email__ = 'denis.cornehl@gmail.com'
-__version__ = '0.5.0'
-
 
 try:
     from .closeio import CloseIO, CloseIOError  # noqa
--- setup.py
+++ setup.py
@@ -1,56 +1,8 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-with open('README.rst') as f:
-    readme = f.read()
-with open('HISTORY.rst') as f:
-    history = f.read().replace('.. :changelog:', '')
-PACKAGE = __import__('closeio')
-NAME = 'faster_closeio'
-
-URL = 'https://github.com/Thermondo/closeio'
-
+from setuptools import setup
 
 setup(
-    name=NAME,
-    version=PACKAGE.__version__,
-    description='Slim API wrapper to access close.io CRM',
-    long_description=readme + '\n\n' + history,
-    author=PACKAGE.__author__,
-    author_email=PACKAGE.__email__,
-    download_url=URL,
-    url=URL,
-    packages=[
-        'closeio',
-        'closeio.contrib',
-        'closeio.contrib.django',
-    ],
-    package_dir={'closeio': 'closeio'},
-    include_package_data=True,
-    install_requires=[
-        'requests',
-        'slumber',
-        'python-dateutil',
-        'six',
-    ],
-    license="APL2",
-    zip_safe=False,
-    keywords='closeio',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
-    entry_points={'pytest11': ['closeio = closeio.contrib.pytest_plugin']},
+    setup_requires=['pbr'],
+    pbr=True,
 )
