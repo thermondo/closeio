38c559e10b824c04278eb786e2c7aac51563896a
Anne Fleischer <anneFly@users.noreply.github.com>
2018-11-12 11:09:07 +0100

    cleanup dev and test configuration (#133)

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 from setuptools import setup
 
 setup(
-    setup_requires=['pbr'],
+    setup_requires=['pbr', 'pytest-runner'],
+    tests_require=['django>=1.11', 'pytest', 'pytest-django', 'pytest-rerunfailures'],
     pbr=True,
 )
