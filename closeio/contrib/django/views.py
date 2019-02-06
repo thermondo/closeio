37b566a8fe414c5cdf0e70f1b2e3ac12cc125105
Ana Paula Gomes <apgomes88@gmail.com>
2019-02-06 16:23:32 +0100

    Support new and legacy version of webhooks API

--- closeio/contrib/django/views.py
+++ closeio/contrib/django/views.py
@@ -15,9 +15,34 @@ logger = logging.getLogger(__name__)
 
 
 class CloseIOWebHook(View):
+    # first elements are from webhook legacy API
+    created = ['create', 'created']
+    updated = ['update', 'updated']
+    deleted = ['delete', 'deleted']
+    merged = ['merge', 'merged']
+
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
         return super(CloseIOWebHook, self).dispatch(*args, **kwargs)
+
+    @staticmethod
+    def _from_legacy(query):
+        return isinstance(query.get('event'), str)
+
+    def _get_event(self, query):
+        if self._from_legacy(query):
+            return query['event']
+        return query['event']['action']
+
+    def _get_model(self, query):
+        if self._from_legacy(query):
+            return query['model']
+        return query['event']['object_type']
+
+    def _get_data(self, query):
+        if self._from_legacy(query):
+            return query['data']
+        return query['event']['data']
 
     def post(self, request, *args, **kwargs):
         try:
@@ -27,9 +52,9 @@ class CloseIOWebHook(View):
             return HttpResponseBadRequest()
 
         try:
-            event = query['event']
-            model = query['model']
-            data = utils.parse(query['data'])
+            event = self._get_event(query)
+            model = self._get_model(query)
+            data = utils.parse(self._get_data(query))
         except KeyError:
             logger.exception("CloseIO webhook request could not be dispatched.")
             return HttpResponseBadRequest()
@@ -38,21 +63,21 @@ class CloseIOWebHook(View):
             instance=data
         )
 
-        if event == 'create':
+        if event in self.created:
             signals.closeio_create.send(
                 sender=self.__class__,
                 model=model,
                 **data_to_send
             )
 
-        elif event == 'update':
+        elif event in self.updated:
             signals.closeio_update.send(
                 sender=self.__class__,
                 model=model,
                 **data_to_send
             )
 
-        elif event == 'delete':
+        elif event in self.deleted:
             data_to_send = dict(
                 instance_id=data.get('id', '')
             )
@@ -62,7 +87,7 @@ class CloseIOWebHook(View):
                 **data_to_send
             )
 
-        elif event == 'merge':
+        elif event in self.merged:
             data_to_send = dict(
                 source_id=data.get('source_id', ''),
                 destination_id=data.get('destination_id', ''),
