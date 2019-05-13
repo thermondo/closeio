12ddbd6ca1d82427e22398aefd839b3603b00aaf
Anne Fleischer <anne.fleischer@thermondo.de>
2019-05-13 15:05:45 +0200

    add utils for webhook signature verification

---
+++ closeio/contrib/django/utils.py
@@ -0,0 +1,69 @@
+import hashlib
+import hmac
+import json
+import logging
+
+from django.conf import settings
+
+logger = logging.getLogger(__name__)
+
+
+def webhook_signature_valid(request):
+    """Verifies the integrity of signed webhooks from Closeio.
+
+    When a webhook subscription is created via the Closeio API you get a secret
+    signature key in the response of the request. This is the key that Closeio
+    uses to sign their webhooks. You can use the key to verify that the request
+    is indeed coming from Closeio (only Closeio should have the signature key)
+    and that the payload was not altered inbetween.
+    This is a simple security mechanism to protect you from false requests to your
+    endpoints that receive the webhooks.
+
+    The signature key for each webhook subscription needs to be stored inside the
+    environment variable `CLOSEIO_WEBHOOK_SIGNATURE_KEYS` as json string containing
+    the webhook subscription ID as key and the signature key as value, e.g.::
+        CLOSEIO_WEBHOOK_SIGNATURE_KEYS = '{"whsub_1": "123", "whsub_2": "456"}'
+
+    More information can be found in the `Closeio Docs`_ in the section "Webhook signatures".
+
+    .. _Closeio Docs: https://developer.close.com/#webhook
+
+    Args:
+        request: an instance of Django's ``HttpRequest`` object
+    """
+    payload = json.loads(request.body)
+    subscription_id = payload.get('subscription_id')
+    if not subscription_id:
+        return False
+
+    try:
+        signature_keys = json.loads(settings.CLOSEIO_WEBHOOK_SIGNATURE_KEYS)
+    except AttributeError:
+        logger.warning('CLOSEIO_WEBHOOK_SIGNATURE_KEYS setting not set.')
+        return False
+    except TypeError:
+        logger.warning('Cannot parse value of CLOSEIO_WEBHOOK_SIGNATURE_KEYS to json.')
+        return False
+
+    try:
+        signature_key = signature_keys[subscription_id]
+    except KeyError:
+        logger.warning('No signature key set for Closeio webhook subscription with id %s.',
+                       subscription_id)
+        return False
+
+    close_sig_hash = request.META.get('HTTP_CLOSE_SIG_HASH')
+    close_sig_timestamp = request.META.get('HTTP_CLOSE_SIG_TIMESTAMP')
+
+    if not close_sig_hash or not close_sig_timestamp:
+        return False
+
+    data = close_sig_timestamp + request.body.decode('utf-8')
+    signature = hmac.new(
+        bytearray.fromhex(signature_key),
+        data.encode('utf-8'),
+        hashlib.sha256
+    ).hexdigest()
+    valid = hmac.compare_digest(close_sig_hash, signature)
+
+    return valid
--- closeio/contrib/pytest_plugin.py
+++ closeio/contrib/pytest_plugin.py
@@ -1,4 +1,10 @@
+import datetime
+import hashlib
+import hmac
+import json
 import random
+import secrets
+import string
 
 import pytest
 
@@ -24,3 +30,43 @@ def opportunity_status(closeio, random_string):
     yield os
 
     # closeio.delete_lead_status(ls['id'])
+
+
+@pytest.fixture
+def sign_closeio_webhook_request(settings):
+    """Sign requests for endpoints that expect webhooks from Closeio.
+
+    Sets correct settings, alters the payload and computes headers so that fake
+    requests to webhook endpoints pass signature validation.
+
+    Usage::
+        data = {'some key': 'some value'}
+        data, headers = _sign_request(data)
+        client.post('/my/url/', data, **headers)
+    """
+    def _sign_request(payload):
+        webhook_id = 'whsub_' + ''.join(random.choice(string.ascii_letters) for i in range(22))
+        signature_key = secrets.token_hex(32)
+        settings.CLOSEIO_WEBHOOK_SIGNATURE_KEYS = json.dumps({webhook_id: signature_key})
+
+        payload.update({'subscription_id': webhook_id})
+
+        timestamp = str(int(datetime.datetime.now().timestamp()))
+        payload = json.dumps(payload)
+        data = timestamp + payload
+
+        sig_hash = hmac.new(
+            bytearray.fromhex(signature_key),
+            data.encode('utf-8'),
+            hashlib.sha256
+        ).hexdigest()
+
+        headers = {
+            'HTTP_CLOSE_SIG_HASH': sig_hash,
+            'HTTP_CLOSE_SIG_TIMESTAMP': timestamp,
+            'content_type': 'application/json',
+        }
+
+        return payload, headers
+
+    return _sign_request
