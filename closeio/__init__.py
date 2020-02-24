70ab943affa3b495633458ee79efc64acf7ec49c
Denis Cornehl <denis.cornehl@gmail.com>
2020-02-24 10:50:09 +0100

    deprecate this library

--- closeio/__init__.py
+++ closeio/__init__.py
@@ -2,3 +2,6 @@ try:
     from .closeio import CloseIO, CloseIOError  # noqa
 except ImportError:
     pass
+
+import warnings
+warnings.warn('Important: faster_closeio is no longer maintained. Please use the official close.io library instead: https://pypi.org/project/closeio/', DeprecationWarning)
